8da9b038444ac0cac696fe3ee370715374d1880c
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2022-10-15 16:05:11 -0400

    remove secrets

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const bodyParser = require('body-parser');
 const { MessagingResponse } = require('twilio').twiml;
 
 const app = express();
+
 const users = {
-  ['+16473891211']: 'Orry'
-}
+  ['+16473891211']: 'Orry',
+};
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
-  const phoneNumber = req.body.From
-  const player = users[phoneNumber] || ''
+  const phoneNumber = req.body.From;
+  const player = users[phoneNumber] || '';
   if (req.body.Body == '1') {
     twiml.message(`See ya tonight ${player}! 🏀`);
   } else if (req.body.Body == '2') {
