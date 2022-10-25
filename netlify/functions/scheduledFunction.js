ca1663a2c65a77e889acce787fad26a48020c0e3
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2022-10-24 23:52:02 -0400

    clean schedule function

--- netlify/functions/scheduledFunction.js
+++ netlify/functions/scheduledFunction.js
@@ -1,8 +1,21 @@
 const { schedule } = require('@netlify/functions');
-const sendMessage = require('../../scripts/sendMessage');
+const twilio = require('twilio');
+require('dotenv').config();
+
+const accountSid = process.env.accountSid; // Your Account SID from www.twilio.com/console
+const authToken = process.env.authToken; // Your Auth Token from www.twilio.com/console
+
+const client = new twilio(accountSid, authToken);
 
 const handler = async function (event, context) {
-  await sendMessage();
+  client.messages
+    .create({
+      body: 'Are you in for ball tonight? Type "1" for yes, or "2" for no.',
+      to: process.env.toPhone,
+      from: process.env.fromPhone, // From a valid Twilio number
+    })
+    .then(message => console.log(message))
+    .catch(err => console.log(err));
 };
 
 module.exports.handler = schedule('0 12 * * *', handler);
