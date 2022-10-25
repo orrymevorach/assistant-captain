ef267cee2b760d4c6cdaba5554f4d74bdf622d46
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2022-10-24 23:46:34 -0400

    try making a small change

--- netlify/functions/scheduledFunction.js
+++ netlify/functions/scheduledFunction.js
@@ -3,9 +3,6 @@ const sendMessage = require('../../scripts/sendMessage');
 
 const handler = async function (event, context) {
   await sendMessage();
-  return {
-    statusCode: 200,
-  };
 };
 
 module.exports.handler = schedule('* * * * *', handler);
