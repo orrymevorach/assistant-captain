7497d32b735b6ad8d2440baf0df62865f8971e23
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2022-12-14 22:43:03 -0500

    clean up auth state change

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,5 +1,4 @@
 'use client';
-import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { initFirebaseAuth } from '../firebase/config';
 import Cookies from 'js-cookie';
@@ -15,35 +14,19 @@ const App = () => {
   initFirebaseAuth();
   const router = useRouter();
   const provider = new GoogleAuthProvider();
-  const [user, setUser] = useState(null);
   const auth = getAuth();
 
   onAuthStateChanged(auth, user => {
     if (user) {
-      const uid = user.uid;
+      Cookies.set('uid', user.uid);
       router.push('/dashboard');
-    } else {
-      console.log('user is siged out');
     }
   });
 
-  useEffect(() => {
-    if (user) {
-      router.push('/dashboard');
-    }
-  }, [user, router]);
-
   const handleLogin = () => {
     signInWithPopup(auth, provider)
-      .then(result => {
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const user = result.user;
-        Cookies.set('uid', user.uid);
-        setUser(user);
-      })
-      .catch(error => {
-        console.log('error', error);
-      });
+      .then(result => {})
+      .catch(error => console.log('error', error));
   };
 
   return (
