6673f86ceaa53ff33151ac9375fdebf627c4f651
Andrew Boyle <andrew@Andrews-Laptop.local>
2022-12-14 21:13:58 -0500

    removed unnecessary return

--- frontend/firebase/config.js
+++ frontend/firebase/config.js
@@ -13,8 +13,6 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
-export const initFirebaseAuth = () => {
-  return app;
-}
+export const initFirebaseAuth = () => app;
 
 export const auth = getAuth(app);
