ce9eec8e188401920e8859fb955a58d62c3618ed
Andrew Boyle <andrew@Andrews-Laptop.local>
2022-12-15 16:58:43 -0500

    modified middleware to track route changes only for specific routes... also removed the temp cookie creation. The login page now handles the cookie creation after the signInWithPopup runs

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,20 +1,31 @@
 import { NextResponse } from "next/server";
+import { auth } from "./firebase/config";
+import { getAuth, getRedirectResult } from "firebase/auth";
+
+
+
+export const config = {
+    matcher: [
+      '/',
+      '/dashboard',
+    //   '/login'
+    ],
+  }
 
 export const middleware = (req) => {
-    let verify = req.cookies.get("isLoggedIn");
+    
+    let verify = req.cookies.get("uid");
     let url = req.url
     
-    if (verify === undefined) {
-        let res = NextResponse.next();
-        res.cookies.set('firstTimeUsingAppCookie', 'false');
-        verify = res.cookies.get('firstTimeUsingAppCookie');
+    if (verify === undefined && url === "http://localhost:3000/") {
+        return;
     }
 
-    if(verify.value === 'false' && url.includes('/dashboard')){
+    if((verify === undefined || verify.value === 'false') && url.includes('/dashboard')){
         return NextResponse.redirect("http://localhost:3000/");
     }
 
-    if (verify.value === 'true' && url === "http://localhost:3000/") {
+    if (verify.value !== 'false' && url === "http://localhost:3000/") {
       return NextResponse.redirect("http://localhost:3000/dashboard");
     }
 }
