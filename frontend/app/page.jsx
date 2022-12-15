e7a09027bb4031f5c67bbad925b6eae24c402a48
Andrew Boyle <andrew@Andrews-Laptop.local>
2022-12-15 17:19:49 -0500

    removed unused imports and remove use client... component can now be server side

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,9 +1,4 @@
-'use client';
-import { useEffect } from "react";
-import { useRouter } from 'next/navigation';
-import { auth, initFirebaseAuth } from '../firebase/config';
-import { onAuthStateChanged, getRedirectResult } from 'firebase/auth';
-import Cookies from "js-cookie";
+import { initFirebaseAuth } from '../firebase/config';
 import Login from './login/page';
 import styles from './App.module.css';
 
