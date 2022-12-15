5ed25138ddad558d7bf5404c5794dc7ad39f37d7
Andrew Boyle <andrew@Andrews-Laptop.local>
2022-12-15 17:06:57 -0500

    Dashboard component now checks if the users uid matches the firebase auth uid. If theres a mismatch the user will be logged out snd require to sign back in.

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -1,33 +1,38 @@
 "use client";
-import Logout from '../../components/Logout';
+import Logout from '../../components/LogoutButton';
 import { useEffect, useState } from 'react';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
+import { auth } from '../../firebase/config';
+import Cookies from 'js-cookie';
 
 const Dashboard = () => {
-    const [ user, setUser ] = useState(null);
-    const auth = getAuth();
-    
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
+    const router = useRouter();
+   const [ userData, setUserData ] = useState();
+
+   useEffect(() => {
+        onAuthStateChanged(auth, user => {
             if (user) {
-                const userData = {
-                    displayName: user.displayName,
-                    email: user.email,
-                    uid: user.uid,
+                const uidVerify = Cookies.get('uid');
+                if (uidVerify !== user.uid) {
+                    Cookies.set('uid', 'false');
+                    router.push('/')
+                } else {
+                    setUserData(user);
                 }
-                setUser(userData)
-            }
+            } 
         })
-    }, [])
+   }, [])
     
+
     return (
         <div className='parent'>
             {
-                user && 
+                userData && 
                 <div>
-                    <h1>{ `Welcome to the dashboard ${user.displayName}` }</h1>
-                    <p>{ `Email: ${user.email}` }</p>
-                    <p>{ `UID: ${user.uid}` }</p>
+                    <h1>{ `Welcome to the dashboard` }</h1>
+                    <p>{ `Email: ${userData.email}` }</p>
+                    <p>{ `UID: ${userData.uid}` }</p>
                     <Logout />
                 </div>
             }
