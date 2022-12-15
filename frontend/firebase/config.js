7f75354c1f91b324d7460b39046bfa8f57d58adc
Andrew Boyle <andrew@Andrews-Laptop.local>
2022-12-15 10:12:51 -0500

    needed NEXT_PUBLIC in the name for the client to access the variables

--- frontend/firebase/config.js
+++ frontend/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.FIREBASE_PUBLIC_DOMAIN,
-  projectId: process.env.FIREBASE_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_PUBLIC_MSG_SENDER_ID,
-  appId: process.env.FIREBASE_PUBLIC_APP_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MSG_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
