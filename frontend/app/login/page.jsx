f3a7f05d259529b125d9a88644adc20d3ade5030
Andrew Boyle <andrew@Andrews-Laptop.local>
2022-12-15 17:08:19 -0500

    Login component now uses the signInWithPopUp to handle the auth verification. The Pop up doesn't cause the application to refresh anymore so less logic is required to handle routing.

--- frontend/app/login/page.jsx
+++ frontend/app/login/page.jsx
@@ -1,16 +1,19 @@
 'use client';
-import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
+import { GoogleAuthProvider, signInWithPopup, signInWithRedirect } from "firebase/auth";
 import { auth } from "../../firebase/config";
-import loginStyles from "../../styles/Login.module.css";
+import loginStyles from "./Login.module.css";
 
 const Login = () => {
     const provider = new GoogleAuthProvider();
+    const router = useRouter();
 
     const signIn = async () => {
         try {
-            Cookies.set('isLoggedIn', 'true');
-            await signInWithRedirect(auth, provider);
+            const { user: { uid } } = await signInWithPopup(auth, provider);
+            Cookies.set('uid', uid);
+            router.push('/dashboard');
         } catch(err) {
             console.log(err);
         }
