417462eeaa80c4e08abd798cdea98415fb8831ce
Andrew Boyle <andrew@Andrews-Laptop.local>
2022-12-15 17:05:22 -0500

    onAuthStateChanged was removed since we don't need to track the state change due to the signInWithPopUp from the login component not causing the application to refresh. Routing the user is now handled by the Login component and middleware

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -2,25 +2,13 @@
 import { useEffect } from "react";
 import { useRouter } from 'next/navigation';
 import { auth, initFirebaseAuth } from '../firebase/config';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged, getRedirectResult } from 'firebase/auth';
 import Cookies from "js-cookie";
 import Login from './login/page';
-import styles from '../styles/Home.module.css';
+import styles from './App.module.css';
 
 const App = () => {
     initFirebaseAuth();
-    const router = useRouter();
-
-    useEffect(() => {
-        onAuthStateChanged(auth, (userData) => {
-        if (userData) {
-            Cookies.set('isLoggedIn', `${userData.emailVerified}`);
-            router.push('/dashboard');
-        } else {
-            Cookies.set('isLoggedIn', 'false');
-        }
-    })
-    }, []);
 
     return (
         <div className={styles.container}>
