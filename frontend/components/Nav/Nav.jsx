6a70d2678cc58f6670e7e878ed47216c0a6c13e1
Andrew Boyle <andrew@Andrews-Laptop.local>
2022-12-27 15:06:40 -0500

    Nav now links to appropriate components. The nav is also highlighted depending on which route is selected

--- frontend/components/Nav/Nav.jsx
+++ frontend/components/Nav/Nav.jsx
@@ -1,20 +1,28 @@
 'use client';
-
+import Link from 'next/link';
+import { useLayoutEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
 import LogoutButton from '../LogoutButton';
 import navStyle from './Nav.module.css';
 
 const Nav = () => {
+    const router = usePathname();
+    const [ isSelected, setIsSelected ] = useState('');
+    
+    useLayoutEffect(() => {
+        setIsSelected(router);
+    }, [router])
     return (
         <nav className={navStyle.container}>
             <ul>
-                <li>
-                    Dashboard
+                <li className={isSelected === '/dashboard' ? navStyle.highlighted : undefined}>
+                    <Link href='/dashboard'>Dashboard</Link>
                 </li>
-                <li>
-                    Teams
+                <li className={isSelected === '/dashboard/teams' ? navStyle.highlighted : undefined}>
+                    <Link href='/dashboard/teams'>Teams</Link>
                 </li>
-                <li>
-                    Create A Team
+                <li className={isSelected === '/dashboard/create-a-team' ? navStyle.highlighted : undefined}>
+                    <Link href='/dashboard/create-a-team'>Create A Team</Link>
                 </li>
                 <li>
                     <LogoutButton />
