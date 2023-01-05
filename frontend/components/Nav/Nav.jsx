46a8ff200eb919d38b0ff14b1a2a958da5ab0964
Andrew Boyle <andrew@Andrews-Laptop.local>
2023-01-05 14:40:23 -0500

    an array of nav routes created. navRoutes are mapped instead of statically generated.

--- frontend/components/Nav/Nav.jsx
+++ frontend/components/Nav/Nav.jsx
@@ -1,35 +1,36 @@
 'use client';
 import Link from 'next/link';
-import { useLayoutEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import LogoutButton from '../LogoutButton';
 import navStyle from './Nav.module.css';
 
 const Nav = () => {
     const router = usePathname();
-    const [ isSelected, setIsSelected ] = useState('');
-    
-    useLayoutEffect(() => {
-        setIsSelected(router);
-    }, [router])
+    const navRoutes = [
+        { name: 'Dashboard', route: '/dashboard' },
+        { name: 'Teams', route: '/dashboard/teams' },
+        { name: 'Create A Team', route: '/dashboard/create-a-team' },
+    ];
+
     return (
         <nav className={navStyle.container}>
             <ul>
-                <li className={isSelected === '/dashboard' ? navStyle.highlighted : undefined}>
-                    <Link href='/dashboard'>Dashboard</Link>
-                </li>
-                <li className={isSelected === '/dashboard/teams' ? navStyle.highlighted : undefined}>
-                    <Link href='/dashboard/teams'>Teams</Link>
-                </li>
-                <li className={isSelected === '/dashboard/create-a-team' ? navStyle.highlighted : undefined}>
-                    <Link href='/dashboard/create-a-team'>Create A Team</Link>
-                </li>
+                {navRoutes.map(({ name, route }, idx) => {
+                    return (
+                        <li
+                            className={route === router ? navStyle.highlighted : undefined}
+                            key={`${name}${idx}`}
+                        >
+                            <Link href={route}>{name}</Link>
+                        </li>
+                    );
+                })}
                 <li>
                     <LogoutButton />
                 </li>
             </ul>
         </nav>
-    )
-}
+    );
+};
 
 export default Nav;
