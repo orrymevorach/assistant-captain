d8750bc1ed7edb159034748ce79dc441d1332c5f
Andrew Boyle <andrew@Andrews-Laptop.local>
2023-01-05 15:22:29 -0500

    code refactor

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -4,55 +4,35 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { useRouter } from 'next/navigation';
 import { auth } from '../../firebase/config';
 import Cookies from 'js-cookie';
-import { client, FIND_USER, CREATE_USER } from '../../gql';
+import { getUser, createUser } from '../../airtable/utils';
 
 const Dashboard = () => {
     const router = useRouter();
-    
+
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, user => {
+        const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
             const cookieUid = Cookies.get('uid');
-            // If the user doesn't exist or the users uid stored doesn't match
-            // firebases uid then remove the cookie and redirect the user to login page
-            if(!user || cookieUid !== user.uid) {
+
+            if (!firebaseUser || cookieUid !== firebaseUser.uid) {
                 Cookies.remove('uid');
                 router.push('/');
             }
 
-            if (user) {
-                const { uid } = user;
-                // Check if user exists in database
-                client.query({
-                    query: FIND_USER,
-                    variables: { uid },
-                }).then( res => {
-                    // If user doesn't exist then create new user
-                    if (res.data.users.length === 0) {
-                        const { email } = user
-                        client.mutate({
-                            mutation: CREATE_USER,
-                            variables: { email, uid },
-                            // Update apollo cache with new user created
-                            refetchQueries: [
-                                {
-                                    query: FIND_USER,
-                                    variables: { uid },
-                                }
-                            ]
-                        }).then( res => console.log(res));
-                    }
-                });
+            if (firebaseUser) {
+                const { uid, email } = firebaseUser;
+                const doesUserExist = await getUser(uid);
+                const hasAirtableRecord = doesUserExist.users.length !== 0;
+
+                if (!hasAirtableRecord) {
+                    await createUser(email);
+                }
             }
         });
-    
+
         return () => unsubscribe();
     }, []);
 
-    return (
-        <div className='container'>
-            DASHBOARD
-        </div>
-    )
-}
+    return <div className='container'>DASHBOARD</div>;
+};
 
 export default Dashboard;
