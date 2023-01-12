1bed08aa166127f81b9efa64047af9640e439d39
Andrew Boyle <andrew@Andrews-Laptop.local>
2023-01-12 18:44:24 -0500

    create team form creates a new team record for the user upon submitting form.

--- frontend/app/teams/create-a-team/page.jsx
+++ frontend/app/teams/create-a-team/page.jsx
@@ -1,16 +1,20 @@
 'use client';
-import { createTeam } from '../../../airtable/utils';
+import { createTeam, getUser } from '../../../airtable/utils';
 import createStyle from './CreateTeam.module.css';
 import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
 
 const CreateTeam = () => {
+    const uid = Cookies.get('uid');
     const router = useRouter();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const input = e.target.teamName.value;
-        await createTeam(input);
-        router.push('/dashboard/teams');
+        const getUsersId = await getUser(uid);
+        const { id } = getUsersId.users[0];
+        await createTeam(input, id);
+        router.push('/teams');
     };
 
     return (
