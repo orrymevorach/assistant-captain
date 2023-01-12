3b0ff5f6d78cee1ee2e37819097eae2b2875cba8
Andrew Boyle <andrew@Andrews-Laptop.local>
2023-01-12 18:45:06 -0500

    renamed component to TeamLayout

--- frontend/app/teams/layout.js
+++ frontend/app/teams/layout.js
@@ -1,7 +1,7 @@
 import Nav from '../../components/Nav/Nav';
 import teamsStyle from './Teams.module.css';
 
-export default function DashboardLayout({ children }) {
+export default function TeamLayout({ children }) {
     return (
         <div className={teamsStyle.grid}>
             <Nav />
