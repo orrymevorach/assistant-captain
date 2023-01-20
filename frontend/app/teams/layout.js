535ec252e1d782d77dad057b0be9949aca09fab7
Andrew Boyle <andrew@Andrews-Laptop.local>
2023-01-20 01:44:56 -0500

    context now used to update teamslist from teams component to the nav

--- frontend/app/teams/layout.js
+++ frontend/app/teams/layout.js
@@ -1,11 +1,19 @@
+'use client';
+import { useState, createContext } from 'react';
 import Nav from '../../components/Nav/Nav';
+// import Teams from './page';
 import teamsStyle from './Teams.module.css';
 
+export const TeamsContext = createContext(null);
+
 export default function TeamLayout({ children }) {
+    const [teamList, setTeamList] = useState([]);
     return (
         <div className={teamsStyle.grid}>
-            <Nav />
-            {children}
+            <TeamsContext.Provider value={{ teamList, setTeamList }}>
+                <Nav teams={teamList} />
+                {children}
+            </TeamsContext.Provider>
         </div>
     );
 }
