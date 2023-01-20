ac9890fdde5bf6870ecf858e6bafda40d7a1b19b
Andrew Boyle <andrew@Andrews-Laptop.local>
2023-01-20 01:41:35 -0500

    findteam function created

--- frontend/airtable/utils/index.js
+++ frontend/airtable/utils/index.js
@@ -1,4 +1,4 @@
-import { client, FIND_USER, CREATE_USER, CREATE_TEAM } from '../../gql';
+import { client, FIND_USER, CREATE_USER, CREATE_TEAM, FIND_TEAM } from '../../gql';
 
 export const getUser = async (uid) => {
     try {
@@ -23,18 +23,38 @@ export const createUser = async (uid, email) => {
                     variables: { uid },
                 },
             ],
+            awaitRefetchQueries: true,
         });
     } catch (error) {
         console.log(error);
     }
 };
 
-export const createTeam = async (teamName, id) => {
+export const createTeam = async (teamName, id, uid) => {
     try {
         await client.mutate({
             mutation: CREATE_TEAM,
             variables: { teamName, id },
+            refetchQueries: [
+                {
+                    query: FIND_USER,
+                    variables: { uid },
+                },
+            ],
+            awaitRefetchQueries: true,
         });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const findTeam = async (teamId) => {
+    try {
+        const { data } = await client.query({
+            query: FIND_TEAM,
+            variables: { teamId },
+        });
+        return data.teams[0];
     } catch (error) {
         console.error(error);
     }
