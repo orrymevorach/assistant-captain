a48a2a0c07e7fa7cd2c3de3030eb993b462288b0
Andrew Boyle <andrew@Andrews-Laptop.local>
2023-01-20 01:43:59 -0500

    Teams li now has a sub ul

--- frontend/components/Nav/Nav.jsx
+++ frontend/components/Nav/Nav.jsx
@@ -1,30 +1,48 @@
 'use client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useContext } from 'react';
+import { TeamsContext } from '../../app/teams/layout';
 import LogoutButton from '../LogoutButton';
 import navStyle from './Nav.module.css';
 
 const Nav = () => {
     const router = usePathname();
+    const { teamList } = useContext(TeamsContext);
     const navRoutes = [
         { name: 'Teams', route: '/teams' },
         { name: 'Create A Team', route: '/teams/create-a-team' },
     ];
-    console.log(`NAV`);
+
     return (
         <nav className={navStyle.container}>
-            <ul>
+            <ul className={navStyle.mainUl}>
                 {navRoutes.map(({ name, route }, idx) => {
                     return (
-                        <li
-                            className={route === router ? navStyle.highlighted : undefined}
-                            key={`${name}${idx}`}
-                        >
-                            <Link href={route}>{name}</Link>
+                        <li className={navStyle.mainLi} key={`${name}${idx}`}>
+                            <Link
+                                href={route}
+                                className={`${navStyle.mainLink} ${
+                                    route === router ? navStyle.highlighted : undefined
+                                }`}
+                            >
+                                {name}
+                            </Link>
+                            {teamList.length !== 0 && name === 'Teams' && (
+                                <ul className={navStyle.subUl}>
+                                    {teamList.map((team, idx) => {
+                                        return (
+                                            <li key={`${team}${idx}`}>
+                                                <Link href={`/teams/${team.id}`}>{team.name}</Link>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            )}
                         </li>
                     );
                 })}
-                <li>
+                <li className={navStyle.mainLi}>
                     <LogoutButton />
                 </li>
             </ul>
