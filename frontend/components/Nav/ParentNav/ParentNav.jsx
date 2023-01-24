153fdc58879b7bf1d11c5722f3133d3978fcc31e
Andrew Boyle <a.michael.boyle@gmail.com>
2023-01-24 18:39:26 -0500

    parent nav dir now holds the nav

---
+++ frontend/components/Nav/ParentNav/ParentNav.jsx
@@ -0,0 +1,36 @@
+'use client';
+import { useContext } from 'react';
+import { TeamsContext } from '../../../app/teams/layout';
+import LogoutButton from '../../LogoutButton';
+import NavLink from '../NavLink';
+import SubNav from '../SubNav/SubNav';
+import navStyle from './ParentNav.module.css';
+
+const ParentNav = () => {
+  const { teamList } = useContext(TeamsContext);
+
+  const navRoutes = [
+    { name: 'Teams', route: '/teams' },
+    { name: 'Create A Team', route: '/teams/create-a-team' },
+  ];
+
+  return (
+    <nav className={navStyle.container}>
+      <ul className={navStyle.list}>
+        {navRoutes.map(({ name, route }, idx) => {
+          return (
+            <li className={navStyle.item} key={`${name}${idx}`}>
+              <NavLink routes={{ name, route }} />
+              {name === 'Teams' && teamList.length !== 0 && <SubNav />}
+            </li>
+          );
+        })}
+        <li className={navStyle.mainLi}>
+          <LogoutButton />
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default ParentNav;
