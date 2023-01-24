d1496ca1d8fc142c5bbb59dcec65a819ee62ee15
Andrew Boyle <a.michael.boyle@gmail.com>
2023-01-24 12:31:44 -0500

    removed use of let, variable is not being changed. idx removed from key.

--- frontend/components/Player/Player.jsx
+++ frontend/components/Player/Player.jsx
@@ -2,9 +2,9 @@ const Player = ({ player }) => {
   return (
     <div>
       <h1>Members</h1>
-      {player.map((member, idx) => {
-        let email = member.email;
-        return <div key={`${email}${idx}`}>{email}</div>;
+      {player.map((member) => {
+        const email = member.email;
+        return <div key={`${email}`}>{email}</div>;
       })}
     </div>
   );
