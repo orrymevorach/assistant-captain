f825c823a0157a2e125673e8e50387a8a1aec136
Andrew Boyle <a.michael.boyle@gmail.com>
2023-01-31 18:52:57 -0500

    onauthstatechanged moved to layout.js

--- frontend/app/teams/page.jsx
+++ frontend/app/teams/page.jsx
@@ -1,51 +1,23 @@
 'use client';
-import { useContext, useEffect } from 'react';
-import { onAuthStateChanged } from 'firebase/auth';
 import { useRouter } from 'next/navigation';
-import { auth } from '@firebase/config';
-import Cookies from 'js-cookie';
-import { getUser, createUser } from '@airtable/utils';
 import teamsStyle from './Teams.module.css';
-import { TeamsContext } from './layout';
+import { useUser } from '@user-context';
 
 const Teams = () => {
   const router = useRouter();
-  const { teamList, setTeamList } = useContext(TeamsContext);
+  const { user } = useUser();
+  const { teams } = user[0];
 
   const selectTeam = (teamId) => {
     router.push(`/teams/${teamId}`);
   };
 
-  useEffect(() => {
-    const handleLoginOnPageLoad = onAuthStateChanged(auth, async (firebaseUser) => {
-      if (!firebaseUser) {
-        Cookies.remove('uid');
-        router.push('/');
-      }
-
-      if (firebaseUser) {
-        const { uid, email } = firebaseUser;
-        const doesUserExist = await getUser(uid);
-        const hasAirtableRecord = doesUserExist.users.length !== 0;
-
-        if (!hasAirtableRecord) {
-          await createUser(uid, email);
-        } else {
-          const teamArray = doesUserExist.users[0].teams;
-          setTeamList(teamArray);
-        }
-      }
-    });
-
-    return () => handleLoginOnPageLoad();
-  }, []);
-
   return (
     <div className={teamsStyle.container}>
       <h1>Your Teams</h1>
       <div className={teamsStyle.teamContainer}>
-        {teamList.length ? (
-          teamList.map((team, idx) => {
+        {teams.length ? (
+          teams.map((team, idx) => {
             return (
               <div
                 className={teamsStyle.teamCard}
