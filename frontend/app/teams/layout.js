8ff47ccba3e5716fe7a8cf7df87fb07eec55fabf
Andrew Boyle <a.michael.boyle@gmail.com>
2023-01-31 18:52:31 -0500

    on auth state changed was added to the layout.js file. This is needed to retrieve the user from the auth and set the state to be passed to the UserProvider

--- frontend/app/teams/layout.js
+++ frontend/app/teams/layout.js
@@ -1,18 +1,52 @@
 'use client';
-import { useState, createContext } from 'react';
+import { useEffect, useState } from 'react';
+import { auth } from '@firebase/config';
+import { onAuthStateChanged } from 'firebase/auth';
+import Cookies from 'js-cookie';
+import { getUser, createUser } from '@airtable/utils';
 import Nav from '@components/Nav/ParentNav/ParentNav';
 import teamsStyle from './Teams.module.css';
-
-export const TeamsContext = createContext(null);
+import { UserContext } from '@user-context';
+import { useRouter } from 'next/navigation';
+import Loader from '@components/Loader/Loader';
 
 export default function TeamLayout({ children }) {
-  const [teamList, setTeamList] = useState([]);
+  const router = useRouter();
+  const [user, setUser] = useState([]);
+  const value = { user, setUser };
+
+  useEffect(() => {
+    const handleLoginOnPageLoad = onAuthStateChanged(auth, async (firebaseUser) => {
+      if (!firebaseUser) {
+        Cookies.remove('uid');
+        router.push('/');
+      }
+
+      if (firebaseUser) {
+        const { uid, email } = firebaseUser;
+        const doesUserExist = await getUser(uid);
+        const hasAirtableRecord = doesUserExist.users.length !== 0;
+        let user = doesUserExist.users;
+
+        if (!hasAirtableRecord) {
+          const createdUser = await createUser(uid, email);
+          user = createdUser.users;
+        }
+        setUser(user);
+      }
+    });
+
+    return () => handleLoginOnPageLoad();
+  }, []);
+
+  if (!user.length) return <Loader />;
+
   return (
-    <div className={teamsStyle.grid}>
-      <TeamsContext.Provider value={{ teamList, setTeamList }}>
-        <Nav teams={teamList} />
+    <UserContext.Provider value={value}>
+      <div className={teamsStyle.grid}>
+        <Nav />
         {children}
-      </TeamsContext.Provider>
-    </div>
+      </div>
+    </UserContext.Provider>
   );
 }
