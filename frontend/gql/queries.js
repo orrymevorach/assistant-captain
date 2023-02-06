c75f5ffb6675dbbdd8259df417b6662827dc4d8d
Andrew Boyle <a.michael.boyle@gmail.com>
2023-02-06 15:00:55 -0500

    new queries added to search by phone number to see if a user has created an account when adding a phone number to the roster. create dummy user if the phone number doesn't exist in the database, and add player so new members can be added to the roster of a team

--- frontend/gql/queries.js
+++ frontend/gql/queries.js
@@ -1,5 +1,14 @@
 import { gql } from '@apollo/client';
 
+export const CREATE_USER = gql`
+  mutation CreateUser($email: String!, $uid: String!) {
+    insert_users(email: $email, uid: $uid) {
+      email
+      uid
+    }
+  }
+`;
+
 export const FIND_USER = gql`
   query FindUser($uid: String!) {
     users(uid: $uid) {
@@ -16,15 +25,6 @@ export const FIND_USER = gql`
   }
 `;
 
-export const CREATE_USER = gql`
-  mutation CreateUser($email: String!, $uid: String!) {
-    insert_users(email: $email, uid: $uid) {
-      email
-      uid
-    }
-  }
-`;
-
 export const CREATE_TEAM = gql`
   mutation CreateTeam($teamName: String!, $id: [String]) {
     insert_teams(name: $teamName, players: $id, admins: $id) {
@@ -37,13 +37,43 @@ export const CREATE_TEAM = gql`
 export const FIND_TEAM = gql`
   query FindTeam($teamId: String!) {
     teams(id: $teamId) {
+      id
       name
       admins {
         email
         id
       }
       players {
-        email
+        id
+        phoneNumber
+      }
+    }
+  }
+`;
+
+export const CREATE_DUMMY_USER = gql`
+  mutation CreateDummyUser($phoneNumber: String!, $teamId: [String]) {
+    insert_users(phoneNumber: $phoneNumber, teams: $teamId) {
+      id
+      phoneNumber
+    }
+  }
+`;
+
+export const SEARCH_PHONE_NUMBER = gql`
+  query SearchPhoneNumber($phoneNumber: String!) {
+    users(phoneNumber: $phoneNumber) {
+      id
+      phoneNumber
+    }
+  }
+`;
+
+export const ADD_PLAYER = gql`
+  mutation AddPlayer($teamId: String!, $playerArray: [String!]!) {
+    update_teams(id: $teamId, players: $playerArray) {
+      players {
+        id
         phoneNumber
       }
     }
