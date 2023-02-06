eb7c8e41f8d1d879d38141c74a82b1d99c54ea10
Andrew Boyle <a.michael.boyle@gmail.com>
2023-02-06 15:06:45 -0500

    new exports added -> ADD_PLAYERm SEARCH_PHONE_NUMBER, CREATE_DUMMY_USER

--- frontend/gql/index.js
+++ frontend/gql/index.js
@@ -1,2 +1,10 @@
 export { client } from './apollo-config';
-export { FIND_USER, CREATE_USER, CREATE_TEAM, FIND_TEAM } from './queries';
+export {
+  FIND_USER,
+  CREATE_USER,
+  CREATE_TEAM,
+  FIND_TEAM,
+  ADD_PLAYER,
+  SEARCH_PHONE_NUMBER,
+  CREATE_DUMMY_USER,
+} from './queries';
