ec1bfcb387b9ca5d25f2669f7d9cbf9d956254ca
Andrew Boyle <a.michael.boyle@gmail.com>
2023-02-06 15:04:12 -0500

    admin id added as a key

--- frontend/components/Admin/Admin.jsx
+++ frontend/components/Admin/Admin.jsx
@@ -2,8 +2,8 @@ const Admin = ({ admin }) => {
   return (
     <div>
       <h1>Team Captains</h1>
-      {admin.map((admin) => {
-        return <div>{admin.email}</div>;
+      {admin.map(admin => {
+        return <div key={admin.id}>{admin.email}</div>;
       })}
     </div>
   );
