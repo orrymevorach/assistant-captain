f343ffd877eaf36e735388d60421bfac1607abda
Andrew Boyle <a.michael.boyle@gmail.com>
2023-02-06 15:08:06 -0500

    dynamic route changed to client component. teamData state is passed down to children containing information relating to its child component.

--- frontend/app/teams/[team]/page.jsx
+++ frontend/app/teams/[team]/page.jsx
@@ -1,19 +1,42 @@
+'use client';
 import { findTeam } from '@airtable/utils';
+import AddPlayer from '@components/AddPlayer/AddPlayer';
 import Admin from '@components/Admin/Admin';
+import Loader from '@components/Loader/Loader';
 import Player from '@components/Player/Player';
+import { useEffect, useState } from 'react';
 import teamStyle from './Team.module.css';
 
-const TeamPage = async ({ params }) => {
+const TeamPage = ({ params }) => {
   const teamId = params.team;
-  const { name, admins, players } = await findTeam(teamId);
+  const [teamData, setTeamData] = useState([]);
+
+  useEffect(() => {
+    const fetchTeamData = async () => {
+      const { name, admins, players } = await findTeam(teamId);
+      const data = [
+        {
+          name: name,
+          admins: admins,
+          players: players,
+        },
+      ];
+      setTeamData(data);
+    };
+    fetchTeamData();
+  }, []);
+
+  if (!teamData.length) return <Loader />;
+  const { name, admins, players } = teamData[0];
 
   return (
-    <div>
+    <div className={teamStyle.container}>
       <h1>{name.toUpperCase()}</h1>
-      <div className={teamStyle.personContainer}>
+      <div className={teamStyle.members}>
         <Admin admin={admins} />
         <Player player={players} />
       </div>
+      <AddPlayer team={{ teamId, teamData }} setTeamData={setTeamData} />
     </div>
   );
 };
